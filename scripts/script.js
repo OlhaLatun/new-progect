5a56504b7ed24e78164731d2e5564d2482b67504
Olga Latun <olha.latun@gmail.com>
2020-03-02 13:35:37 +0200

    Bulb project

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
-alert("Welcome");
+function lightIsOn() {
+  const bulb = document.getElementsByClassName("fas fa-lightbulb")[0];
+  bulb.style.color = "yellow";
+
+  const text = document.getElementsByTagName("h1")[0];
+  text.innerHTML = "Bulb is: ON";
+  text.style.color = "black";
+
+  const background = document.getElementsByTagName("body")[0];
+  background.style.backgroundColor = "white";
+}
+
+function lightIsOff() {
+  const bulb = document.getElementsByClassName("fas fa-lightbulb")[0];
+  bulb.style.color = "black";
+
+  const text = document.getElementsByTagName("h1")[0];
+  text.innerHTML = "Bulb is: OFF";
+  text.style.color = "white";
+
+  const background = document.getElementsByTagName("body")[0];
+  background.style.backgroundColor = "grey";
+}
