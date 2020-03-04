be0aba496d6e5d90c9a889d3122d61b7183d9211
Olga Latun <olha.latun@gmail.com>
2020-03-04 11:58:53 +0200

    modified bulb project

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,24 @@
+// function lightIsOn() {
+//   const bulb = document.getElementsByClassName("fas fa-lightbulb")[0];
+//   bulb.style.color = "yellow";
+
+//   const text = document.getElementsByTagName("h1")[0];
+//   text.innerHTML = "Bulb is: ON";
+//   text.style.color = "black";
+
+//   const background = document.getElementsByTagName("body")[0];
+//   background.style.backgroundColor = "white";
 function lightIsOn() {
   const bulb = document.getElementsByClassName("fas fa-lightbulb")[0];
-  bulb.style.color = "yellow";
-
-  const text = document.getElementsByTagName("h1")[0];
-  text.innerHTML = "Bulb is: ON";
-  text.style.color = "black";
-
-  const background = document.getElementsByTagName("body")[0];
-  background.style.backgroundColor = "white";
+  const input = document.getElementsByName("setColor")[0];
+  let value;
+  if (input.value) {
+    value = input.value;
+  } else {
+    value = "yellow";
+  }
+  bulb.style.color = value;
 }
-
 function lightIsOff() {
   const bulb = document.getElementsByClassName("fas fa-lightbulb")[0];
   bulb.style.color = "black";
