ca280791f96d681f4373cdc6f1962fb3f90e6b43
Olga Latun <olha.latun@gmail.com>
2020-03-17 16:21:53 +0200

    fixed script loading

--- scripts/matrix.js
+++ scripts/matrix.js
@@ -3,7 +3,9 @@ let importantNotUrgent;
 let notImportantUrgent;
 let notImportantNotUrgent;
 
-(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("document");
+
   importantUrgent = document.getElementById("importantUrgent");
   importantNotUrgent = document.getElementById("importantNotUrgent");
   notImportantUrgent = document.getElementById("notImportantUrgent");
@@ -22,50 +24,50 @@ let notImportantNotUrgent;
       addNewTask(quadrant);
     });
   });
-})();
 
-function addNewTask(quadrant) {
-  console.log(quadrant);
-  const value = getInputValue(quadrant);
-  if (value) {
-    addNewItem(value, quadrant);
-  } else {
-    alert("No task added");
+  function addNewTask(quadrant) {
+    console.log(quadrant);
+    const value = getInputValue(quadrant);
+    if (value) {
+      addNewItem(value, quadrant);
+    } else {
+      alert("No task added");
+    }
   }
-}
 
-function getInputValue(quadrant) {
-  const getValue = quadrant.getElementsByClassName("new-task")[0].value;
-  return getValue;
-}
+  function getInputValue(quadrant) {
+    const getValue = quadrant.getElementsByClassName("new-task")[0].value;
+    return getValue;
+  }
 
-function addNewItem(value, quadrant) {
-  const list = quadrant.getElementsByClassName("toDoList")[0];
-  const newTask = getLiWithText(value);
-  list.appendChild(newTask);
-}
+  function addNewItem(value, quadrant) {
+    const list = quadrant.getElementsByClassName("toDoList")[0];
+    const newTask = getLiWithText(value);
+    list.appendChild(newTask);
+  }
 
-function getLiWithText(value) {
-  const newListItem = document.createElement("li");
-  const checkbox = getCheckbox();
-  const text = document.createTextNode(" " + value);
-  const space = document.createTextNode(" ");
-  const icon = getDeleteIcon();
-  newListItem.appendChild(checkbox);
-  newListItem.appendChild(text);
-  newListItem.appendChild(space);
-  newListItem.appendChild(icon);
-  return newListItem;
-}
+  function getLiWithText(value) {
+    const newListItem = document.createElement("li");
+    const checkbox = getCheckbox();
+    const text = document.createTextNode(" " + value);
+    const space = document.createTextNode(" ");
+    const icon = getDeleteIcon();
+    newListItem.appendChild(checkbox);
+    newListItem.appendChild(text);
+    newListItem.appendChild(space);
+    newListItem.appendChild(icon);
+    return newListItem;
+  }
 
-function getCheckbox() {
-  const input = document.createElement("input");
-  input.type = "checkbox";
-  return input;
-}
+  function getCheckbox() {
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    return input;
+  }
 
-function getDeleteIcon() {
-  const i = document.createElement("i");
-  i.className = "fas fa-trash";
-  return i;
-}
+  function getDeleteIcon() {
+    const i = document.createElement("i");
+    i.className = "fas fa-trash";
+    return i;
+  }
+});
