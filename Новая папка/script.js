b8d7e6557e9cbb4f74bca74136a04613b5836814
Олюня <oluna@iMac-MNE02.local>
2020-04-15 13:47:04 +0300

    got rid of  for  function

--- Новая папка/script.js
+++ Новая папка/script.js
@@ -1,4 +1,12 @@
 
+const divs = [
+    document.querySelector(".class1"),
+    document.querySelector(".class2"),
+    document.querySelector(".class3"),
+    document.querySelector(".class4"),
+]
+
+
     const buttonUp = document.querySelector(".upBtn");
     buttonUp.addEventListener("click", () => {
        upAndDown ("up");
@@ -13,26 +21,79 @@
 function upAndDown (direction) {
     const input = document.querySelector("#input").value.toLowerCase().replace(/\s/g, "");
     const section = document.querySelector("#container");
-    const div = document.querySelector("#container").children;
+    const currentDiv = divs.find(div => div.className == input);
 
     if (direction === "up") {
-        for (let i = 0; i < div.length; i++) {
-          if (i) {
-            if (div[i].className === input) {
-                 section.insertBefore(div[i], div[i - 1]);
-           } 
-        }
+        if (currentDiv.className == input) {
+            section.insertBefore(currentDiv, currentDiv.previousElementSibling);
+           }    
     }
-}
+
     if (direction === "down") {
-        for (let i = 0; i < div.length; i++) {
-          if (i < div.length - 1) {
-            if (div[i].className === input) {
-                 section.insertBefore(div[i + 1], div[i]);
-                 i++;
-           } 
-        }
-    }
-}  
+        if (currentDiv.className == input && currentDiv.nextElementSibling) {
+            section.insertBefore(currentDiv.nextElementSibling, currentDiv); 
+            } else { 
+                section.insertBefore(currentDiv, section.firstElementChild); }
+        }     
+} 
+  
     
-}
+
+
+// function upAndDown (direction) {
+//     const input = document.querySelector("#input").value.toLowerCase().replace(/\s/g, "");
+//     const section = document.querySelector("#container");
+//     const div = document.querySelector("#container").children;
+
+//     if (direction === "up") {
+//         for (let i = 0; i < div.length; i++) {
+//           if (i) {
+//             if (div[i].className === input) {
+//                  section.insertBefore(div[i], div[i - 1]);
+//            } 
+//         }
+//     }
+// }
+//     if (direction === "down") {
+//         for (let i = 0; i < div.length; i++) {
+//           if (i < div.length - 1) {
+//             if (div[i].className === input) {
+//                  section.insertBefore(div[i + 1], div[i]);
+//                  i++;
+//            } 
+//         }
+//     }
+// }  
+    
+// }
+
+// for (var x = "#"; x.length <= 7; x += "#") {
+//     console.log(x);
+// }
+
+
+//  for (let num = 1; num <= 100; num++)  {
+//      let output = "";
+//         if (num % 3 == 0) output += "Fizz";
+//         if (num % 5 == 0) output += "Buzz";
+//         console.log(output || num);
+    
+// }
+
+// let size = 8;
+// let board = "";
+
+// for (let y = 0; y < size; y++) {
+   
+//     for (let x = 0; x < size; x++) {
+        
+//         if ((x + y) % 2 == 0) {
+//             board += " ";
+//         } else {
+//             board += "#";
+//         }
+//     }
+//     board += "\n";
+// }
+
+// console.log(board);
